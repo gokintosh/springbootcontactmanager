473f927c244d098ed160b9e4500ec932bdc649bb
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-28 03:52:57 +0100

    finish homepage

--- src/main/java/com/gokul/contactmanager/controller/HomeController.java
+++ src/main/java/com/gokul/contactmanager/controller/HomeController.java
@@ -3,13 +3,16 @@ package com.gokul.contactmanager.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class HomeController {
 
 
-    @GetMapping("/home")
+
+    @GetMapping("/")
     public String home(Model model){
         model.addAttribute("title","Home page");
         return "home";
@@ -19,6 +22,12 @@ public class HomeController {
     public String about(Model model){
         model.addAttribute("title","About page");
         return "about";
+    }
+
+    @GetMapping("/signup")
+    public String signup(Model model){
+        model.addAttribute("title","SignUp page");
+        return "signup";
     }
 
 }
