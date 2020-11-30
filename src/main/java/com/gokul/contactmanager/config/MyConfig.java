9ebb3fa3572f3cff8ed416d79d148393c7200546
GOKUL KANNAMBALLY BALACHNADRAN <52125327+gokintosh@users.noreply.github.com>
2020-11-30 15:08:00 +0100

    jhfgff

--- src/main/java/com/gokul/contactmanager/config/MyConfig.java
+++ src/main/java/com/gokul/contactmanager/config/MyConfig.java
@@ -3,9 +3,23 @@ package com.gokul.contactmanager.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
-public class MyConfig {
+@EnableWebSecurity
+public class MyConfig extends WebSecurityConfigurerAdapter {
 
+    @Bean
+    public UserDetailsService getUserDetailService(){
+        return new UserDetailsServiceImpl();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 }
